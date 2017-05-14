c231bec24a5017c57780061cb39fdfb1fd11ca53
Mikhail Virovets <virovets64@gmail.com>
2017-05-14 22:08:25 +0300

    (=) Remove unused code

--- BambooLogViewer/MainWindow.xaml.cs
+++ BambooLogViewer/MainWindow.xaml.cs
@@ -26,43 +26,16 @@ namespace BambooLogViewer
     {
       InitializeComponent();
       editPath.Text = Properties.Settings.Default.LastPath;
-//      trvLog.ItemsSource = createSampleData().Builds;
-//      trvLog.ItemsSource = loadLogFile().Builds;
-    }
-
-    BambooLog createSampleData()
-    {
-      var log = new BambooLog();
-      {
-        var build = new Build() { Name = "Build 1" };
-        build.Tasks.Add(new PlanTask() { Name = "Task 11" });
-        build.Tasks.Add(new PlanTask() { Name = "Task 12" });
-        log.Builds.Add(build);
-      }
-      {
-        var build = new Build() { Name = "Build 2" };
-        build.Tasks.Add(new PlanTask() { Name = "Task 21" });
-        log.Builds.Add(build);
-      }
-
-      return log;
-    }
-
-    BambooLog loadLogFile()
-    {
-      var lines = File.ReadLines(@"c:\Temp\Sample.log");
-      return Parser.BambooLogParser.Parse(lines);
     }
 
     private void btnOpen_Click(object sender, RoutedEventArgs e)
     {
-//      var lines = File.ReadLines(editPath.Text);
       Properties.Settings.Default.LastPath = editPath.Text;
       var lines = Parser.BambooLogParser.downloadFile(editPath.Text);
       var logFile = Parser.BambooLogParser.Parse(lines);
       trvLog.ItemsSource = logFile.Builds;
       Properties.Settings.Default.Save();
     }
-     
+    
   }
 }
