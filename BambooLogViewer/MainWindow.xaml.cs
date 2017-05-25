ddf88d9da6a81595e1a280bbd9d52e5bdf77f3bb
Mikhail Virovets <virovets64@gmail.com>
2017-05-25 09:51:14 +0300

    (+) Add scaling feature

--- BambooLogViewer/MainWindow.xaml.cs
+++ BambooLogViewer/MainWindow.xaml.cs
@@ -27,6 +27,12 @@ namespace BambooLogViewer
       Model.TypeMap.Initialize();
       InitializeComponent();
       editPath.Text = Properties.Settings.Default.LastPath;
+      setScaleFactor(Properties.Settings.Default.ScaleFactor);
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      Properties.Settings.Default.Save();
     }
 
     private void btnOpen_Click(object sender, RoutedEventArgs e)
@@ -52,6 +58,45 @@ namespace BambooLogViewer
       var logFileView = new ViewModel.BambooLog(logFile);
       trvLog.ItemsSource = logFileView.Records;
       trvLog.Focus();
+    }
+
+    private double scaleFactor = 0;
+    private const double scaleStep = 0.1;
+    private const double scaleMin = -2;
+    private const double scaleMax = 3;
+
+    private void setScaleFactor(double value)
+    {
+      var scaleTransform = mainPanel.LayoutTransform as ScaleTransform;
+      if (scaleTransform == null)
+      {
+        scaleTransform = new ScaleTransform(1, 1);
+        mainPanel.LayoutTransform = scaleTransform;
+      }
+      if(value >= scaleMin && value <= scaleMax)
+      {
+        scaleFactor = value;
+        var scale = Math.Pow(2, scaleFactor);
+        scaleTransform.ScaleX = scale;
+        scaleTransform.ScaleY = scale;
+      }
+    }
+
+    private void mainPanel_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+      {
+        if (e.Delta > 0)
+        {
+          setScaleFactor(scaleFactor + scaleStep);
+        }
+        else if (e.Delta < 0)
+        {
+          setScaleFactor(scaleFactor - scaleStep);
+        }
+        Properties.Settings.Default.ScaleFactor = scaleFactor;
+        e.Handled = true;
+      }
     }
   }
 }
--- BambooLogViewer/Properties/Settings.Designer.cs
+++ BambooLogViewer/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace BambooLogViewer.Properties {
                 this["LastPath"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double ScaleFactor {
+            get {
+                return ((double)(this["ScaleFactor"]));
+            }
+            set {
+                this["ScaleFactor"] = value;
+            }
+        }
     }
 }
